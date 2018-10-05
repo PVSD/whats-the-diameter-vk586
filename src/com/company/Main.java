25e6923058e816b4254af4cd5d8c8825b76742b9
Valentina Kononenko <valentina@kononenko.net>
2018-10-04 22:05:40 -0400

    done

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        Circle circ1 = new Circle(35.5);
+        System.out.println( circ1.diameter());
     }
 }
